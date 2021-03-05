84796fd66e99757690159d764598d38152ff883b
Alejandro Menocal <alejandro.menocal@exxonmobil.com>
2021-03-04 21:39:40 -0600

    updating app name

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     apps: [
         {
-            name: 'aws-codedeploy',
+            name: 'alejandroblog-deployments',
             script: 'npx',
             args: 'serve -s build -l 3000 -n',
             interpreter: 'none',
